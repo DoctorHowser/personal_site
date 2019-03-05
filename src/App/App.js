7cb3612010ca9d094558c39c4913b7aa93abc437
Dane Smith <doctorhowser@gmail.com>
2019-03-05 16:03:44 -0600

    fix for github

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { AppBar, Typography, Toolbar, Avatar, MuiThemeProvider, createMuiTheme, Paper } from '@material-ui/core';
 import ExternalIconLink from '../ExternalIconLink/ExternalIconLink'
 import './App.css';
